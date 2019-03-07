64096afce15b7af016dcecf85d48fad36c466cd2
Enzo Scaduto <enzo.scaduto@gmail.com>
2019-03-07 11:22:56 +0100

    randomError genere json with random data

---
+++ sources/randomError.c
@@ -0,0 +1,34 @@
+//
+// Created by Scaduto Enzo on 2019-03-07.
+//
+
+#include "randomError.h"
+
+
+char *autoGeneration(int nb) {
+	srand(time(NULL));
+	int i = 0;
+	char json[4096];
+
+	if (nb == 0) {
+		return "";
+	}
+
+	sprintf(json, "{ Id_plane: %s, Type_plane: %s, Nb_failures: %d, failures: [ { ", idPlane[rand()%60], typePlane[rand()%11], nb);
+
+	while (i < nb) {
+		if (i != 0)
+			sprintf(json, "%s,{", json);
+		sprintf(json, "%s Id_failure: %d, Date: %d, Id_component: %d, Level_criticity: %d, Comment_failure_size: 12, Comment_failure: Test Failure }",json,
+			rand()%(0x100D-0x1000) + 0x1000, rand(), rand()%(0x200D-0x2000) + 0x2000, rand()%10);
+		i += 1;
+	}
+	sprintf(json, "%s] }", json);
+	//printf("%s\n", json);
+	return json;
+}
+/*
+int main(int argc, char** argv)
+{
+	autoGeneration(2);
+}*/
---
+++ sources/randomError.h
@@ -0,0 +1,24 @@
+//
+// Created by Scaduto Enzo on 2019-03-07.
+//
+
+#ifndef EMBEDDEDPROJECT_RANDOMERROR_H
+#define EMBEDDEDPROJECT_RANDOMERROR_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "plane.h"
+#include "failure.h"
+
+char *idPlane[61] = {"YA-AAA", "ZA-AAA", "7T-AAA", "C3-AAA", "D2-AAA", "VP-AAA", "V2-AAA", "LV-AAA", "LQ-AAA", "EK-10000", "P4-AAA", "VH-AAA", "OE-AAA", "OE-BAA", "OE-LAA", "OE-VAA", "OE-WAA", "OE-XAA", "OE-0001",
+					"OE-9000", "4K-AZ1", "4K-10000", "C6-AAA", "A9C-AA", "S2-AAA", "8P-AAA", "EW-10000", "EW-100AA", "EW-200PA", "EW-100PJ", "EW-001DA", "EW-0001L", "OO-AAA", "OO-RAA", "OO-BAA", "OO-YAA", "OO-01",
+					"OO-501", "V3-AAA", "TY-AAA", "VP-BAA", "VQ-BAA", "A5-AAA", "CP-1000", "T9-AAA", "E7-AAA", "A2-AAA", "PP-AAA", "PR-AAA", "PS-AAA", "PT-AAA", "PU-AAA", "VP-LAA", "V8-AAA", "V8-AA1",
+					"V8-001", "LZ-AAA", "XT-AAA", "9U-AAA", "XU-AAA", "TJ-AAA"};
+char *typePlane[11] = { "320", "330", "350", "380", "400", "295", "737", "747", "767", "777", "787"};
+
+
+
+#endif //EMBEDDEDPROJECT_RANDOMERROR_H
